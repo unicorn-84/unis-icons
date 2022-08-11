b87f86e76a283b3272a6291d82449814939a321e
unicorn-84 <unicorn-84@ya.ru>
2022-08-11 11:27:19 +0300

    chore: change utils

--- src/svgr/svgr-index-template.cjs
+++ src/svgr/svgr-index-template.cjs
@@ -1,12 +1,28 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const fs = require('fs');
+require('dotenv/config');
 
 const defaultIndexTemplate = (filePaths) => {
   const exportEntries = filePaths.map((filePath) => {
-    const basename = path.basename(filePath, path.extname(filePath));
+    const name = path.basename(filePath, path.extname(filePath));
 
-    return `export { default as ${basename} } from './${basename}.js'`;
+    return `export { default as ${name} } from './${name}.js'`;
   });
+
+  try {
+    const pathArr = process.env.COMPONENTS_DIR.split('/');
+    const PATH = pathArr.slice(0, -1).join('/');
+    const DIR = pathArr.at(-1);
+
+    const data = `export * from './${DIR}/index.js';\n`;
+
+    fs.appendFileSync(path.join(PATH, 'index.ts'), data);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+
   return exportEntries.join('\n');
 };
 
